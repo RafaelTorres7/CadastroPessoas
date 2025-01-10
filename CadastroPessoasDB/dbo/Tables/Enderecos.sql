e96a24814ac3befafe1d1d982019a3a9f10550f5
Rafael <rafaelfst7@outlook.com>
2025-01-10 19:08:31 -0300

    Adicionando Database Project

---
+++ CadastroPessoasDB/dbo/Tables/Enderecos.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Enderecos] (
+    [Id]                INT            NOT NULL,
+    [PessoaId]          INT            NOT NULL,
+    [EnderecoDescricao] NVARCHAR (200) NOT NULL,
+    [Cep]               NVARCHAR (10)  NOT NULL,
+    [Cidade]            NVARCHAR (100) NOT NULL,
+    [Estado]            NVARCHAR (50)  NOT NULL,
+    CONSTRAINT [PK_Enderecos] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Enderecos_Pessoas] FOREIGN KEY ([PessoaId]) REFERENCES [dbo].[Pessoas] ([Id])
+);
+
---
+++ CadastroPessoasDB/dbo/Tables/Pessoas.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Pessoas] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Nome]     NVARCHAR (100) NULL,
+    [Telefone] NVARCHAR (20)  NULL,
+    [Cpf]      NVARCHAR (20)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
